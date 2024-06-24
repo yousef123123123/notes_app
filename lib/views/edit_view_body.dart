2f562393b41acc3e73b90a1cae2aeb4d2a0d9f26
yousef123123123 <youssifgamal123@gmail.com>
2024-06-25 00:15:11 +0300

    edit note view ui done

--- lib/views/edit_view_body.dart
+++ lib/views/edit_view_body.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_TextField.dart';
+import 'package:notes_app/views/widgets/custom_appBar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(
+            title: 'Edit Notes',
+            icon: Icons.check,
+          ),
+          SizedBox(height: 50),
+          CustomTextfield(hint: 'title'),
+          SizedBox(height: 16),
+          CustomTextfield(
+            hint: 'content',
+            maxLines: 5,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/views/widgets/custom_appBar.dart
+++ lib/views/widgets/custom_appBar.dart
@@ -4,19 +4,22 @@ import 'package:flutter/widgets.dart';
 import 'Custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
+  const CustomAppBar({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         Text(
-          'Notes',
-          style: TextStyle(
+          title,
+          style: const TextStyle(
               color: Color.fromARGB(255, 221, 212, 212), fontSize: 30),
         ),
         Spacer(),
-        CustomSearchIcon(),
+        CustomSearchIcon(
+          icon: icon,
+        ),
       ],
     );
   }
